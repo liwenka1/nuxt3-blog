68f55a6f5cc91d0a87a698d58b2cffd983fcfaff
liwenkai <2020583117@qq.com>
2023-07-29 23:43:42 +0800

    feat: 添加导航栏

--- src/layouts/components/header.vue
+++ src/layouts/components/header.vue
@@ -1,20 +1,30 @@
 <template>
-  <header>
-    <label class="swap">
-      <input type="checkbox" @click="toggleDark" />
-      <div class="swap-on">
-        ON
-        <SvgIcon icon-class="moon" />
-      </div>
-      <div class="swap-off">
-        OFF
-        <SvgIcon icon-class="sun" />
-      </div>
-    </label>
-  </header>
+  <div class="navbar bg-base-100">
+    <div class="flex-1">
+      <a class="btn btn-ghost normal-case text-xl">Li WenKai</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal">
+        <li v-for="link in navLinks" :key="link.path">
+          <a>
+            <SvgIcon :icon-class="link.icon" size="1.5em" />
+          </a>
+        </li>
+        <li>
+          <label class="swap">
+            <input type="checkbox" @click="toggleDark" />
+            <SvgIcon class="swap-on" icon-class="moon" size="1.5em" />
+            <SvgIcon class="swap-off" icon-class="sun" size="1.5em" />
+          </label>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { navLinks } from '@/site.config'
+
 let isDark = ref<boolean>(false)
 const toggleDark = () => {
   isDark.value = !isDark.value
